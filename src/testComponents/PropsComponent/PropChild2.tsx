9392c4718bd6991c9c1ed95809de2a271715d1e2
Aren <197610445@qq.com>
2018-12-13 11:13:05 +0800

    React性能优化之不在render中使用bind和箭头函数

--- src/testComponents/PropsComponent/PropChild2.tsx
+++ src/testComponents/PropsComponent/PropChild2.tsx
@@ -4,17 +4,17 @@ declare type GenderType = string;
 interface Ipoprs {
   name: string,
   gender?: GenderType,
-  onChangeName?: (name: string, e: React.MouseEvent<HTMLButtonElement>) => void
+  onChangeName: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 // 第一个为prop,第二个为state
 export default class PropChild1 extends React.Component<Ipoprs> {
   public render(): React.ReactNode{
-    const {name} = this.props
+    const {name, onChangeName} = this.props
     return(
       <div>
         <div>{name}</div>
-        <Button type="default"><Icon type="plus" />click me</Button>
+        <Button type="default" data-name="buton2"  onClick={ onChangeName }><Icon type="plus"/>点击修改姓名2</Button>
         <div>{this.props.gender}</div>
       </div>
     )
--- src/testComponents/PropsComponent/PropComponent.tsx
+++ src/testComponents/PropsComponent/PropComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import PropChild1 from './PropChild1';
+import PropChild2 from './PropChild2';
 import { Button } from 'antd';
 
 interface Isate {
@@ -16,19 +17,45 @@ export default class PropComponent extends React.Component<RouteComponentProps, 
     this.handleChangeName = this.handleChangeName.bind(this)
   }
 
-  public handleChangeName = (name: string, e: Event) => {
-    window.console.log(name)
-    window.console.log(e.target)
+  // 不推荐在render方法中使用箭头函数或者使用bind，会导致重复绑定事件。
+  // 1.将数据传递给HTML元素
+  public handleChangeName = (e: React.MouseEvent<HTMLButtonElement>) => {
+    window.console.log((e.target as HTMLInputElement).getAttribute('data-name'))
+    const name = (e.target as HTMLInputElement).getAttribute('data-name') as string
+    // window.console.log(e.target)
     this.setState({
       name
     })
   }
 
 
+
+  // 2.提取成子组件
+  public handleChangeName2 = (name: string, e: React.MouseEvent<HTMLButtonElement>) => {
+    window.console.log((e.target as HTMLInputElement).getAttribute('data-name'))
+    // window.console.log(e.target)
+    this.setState({
+      name
+    })
+  }
+
+  private _renderChild(): React.ReactNode {
+    return (
+      <>
+        <PropChild2 name={ this.state.name} gender="niupi" onChangeName = {this.handleChangeName2.bind(this, 'xinglun')} />
+      </>
+    )
+  }
+
+
+  // tslint:disable-next-line:member-ordering
   public render(): React.ReactNode {
     return (
       <div>
-        <Button name="xinglun" onClick={this.handleChangeName.bind(null, 'xinglun')}>点击修改姓名</Button>
+        <Button name="xinglun" data-name="dasd" onClick={this.handleChangeName}>点击修改姓名</Button>
+        <hr/>
+        {this._renderChild()}
+        <hr/>
         {/* <Button onClick={(e) => this.handleChangeName(name, e)}>点击修改姓名</Button> */}
         <PropChild1 name={ this.state.name} gender="man" />
       </div>
